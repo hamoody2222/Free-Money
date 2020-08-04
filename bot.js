a6a4fbe040b2ba42c195769c89265477c32a202a
hamoody2222 <68521031+hamoody2222@users.noreply.github.com>
2020-08-04 23:29:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['702449053643964466'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
